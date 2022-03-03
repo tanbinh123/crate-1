63e1187d5aa1f09235b307c4be6a2c6800b96c5f
baur <baurzhansahariev@gmail.com>
2022-03-03 10:42:53 +0100

    use fastutil map with custom hashcode in GroupByMaps

--- server/src/main/java/io/crate/execution/engine/aggregation/GroupByMaps.java
+++ server/src/main/java/io/crate/execution/engine/aggregation/GroupByMaps.java
@@ -30,14 +30,20 @@ import io.crate.types.IntegerType;
 import io.crate.types.LongType;
 import io.crate.types.ShortType;
 import io.crate.types.TimestampType;
-import io.netty.util.collection.ByteObjectHashMap;
-import io.netty.util.collection.IntObjectHashMap;
-import io.netty.util.collection.LongObjectHashMap;
-import io.netty.util.collection.ShortObjectHashMap;
+import it.unimi.dsi.fastutil.bytes.Byte2ObjectOpenCustomHashMap;
+import it.unimi.dsi.fastutil.bytes.ByteHash;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenCustomHashMap;
+import it.unimi.dsi.fastutil.ints.IntHash;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenCustomHashMap;
+import it.unimi.dsi.fastutil.longs.LongHash;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.shorts.Short2ObjectOpenCustomHashMap;
+import it.unimi.dsi.fastutil.shorts.ShortHash;
+import net.jpountz.xxhash.XXHash32;
+import net.jpountz.xxhash.XXHashFactory;
 import org.apache.lucene.util.RamUsageEstimator;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
@@ -50,6 +56,82 @@ public final class GroupByMaps {
         Map.entry(DataTypes.INTEGER, 8),
         Map.entry(DataTypes.LONG, 12)
     );
+
+    private static final XXHash32 xxHash32 = XXHashFactory.fastestInstance().hash32();
+
+    private static final class ByteHashStrategy implements ByteHash.Strategy {
+
+        private static final ByteHashStrategy INSTANCE = new ByteHashStrategy();
+
+        @Override
+        public int hashCode(byte value) {
+            return xxHash32.hash(new byte[]{value}, 0, Byte.BYTES, 0);
+        }
+
+        @Override
+        public boolean equals(byte a, byte b) {
+            return a == b;
+        }
+    }
+
+    private static final class ShortHashStrategy implements ShortHash.Strategy {
+
+        private static final ShortHashStrategy INSTANCE = new ShortHashStrategy();
+
+        @Override
+        public int hashCode(short value) {
+            return xxHash32.hash(new byte[] {
+                (byte)(value >> 8),
+                (byte)value}, 0, Short.BYTES, 0);
+        }
+
+        @Override
+        public boolean equals(short a, short b) {
+            return a == b;
+        }
+    }
+
+    private static final class IntHashStrategy implements IntHash.Strategy {
+
+        private static final IntHashStrategy INSTANCE = new IntHashStrategy();
+
+        @Override
+        public int hashCode(int value) {
+            return xxHash32.hash(new byte[] {
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value}, 0, Integer.BYTES, 0);
+        }
+
+        @Override
+        public boolean equals(int a, int b) {
+            return a == b;
+        }
+    }
+
+    private static final class LongHashStrategy implements LongHash.Strategy {
+
+        private static final LongHashStrategy INSTANCE = new LongHashStrategy();
+
+        @Override
+        public int hashCode(long value) {
+            return xxHash32.hash(new byte[] {
+                (byte)(value >> 56),
+                (byte)(value >> 48),
+                (byte)(value >> 40),
+                (byte)(value >> 32),
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value}, 0, Long.BYTES, 0);
+        }
+
+        @Override
+        public boolean equals(long a, long b) {
+            return a == b;
+        }
+    }
 
     public static <K, V> BiConsumer<Map<K, V>, K> accountForNewEntry(RamAccounting ramAccounting,
                                                                      SizeEstimator<K> sizeEstimator,
@@ -69,21 +151,23 @@ public final class GroupByMaps {
     }
 
     public static <K, V> Supplier<Map<K, V>> mapForType(DataType<K> type) {
+
         switch (type.id()) {
             case ByteType.ID:
-                return () -> (Map) new PrimitiveMapWithNulls<>(new ByteObjectHashMap<>());
+                return () -> new PrimitiveMapWithNulls(new Byte2ObjectOpenCustomHashMap<V>(ByteHashStrategy.INSTANCE));
             case ShortType.ID:
-                return () -> (Map) new PrimitiveMapWithNulls<>(new ShortObjectHashMap<>());
+                return () -> new PrimitiveMapWithNulls(new Short2ObjectOpenCustomHashMap<V>(ShortHashStrategy.INSTANCE));
             case IntegerType.ID:
-                return () -> (Map) new PrimitiveMapWithNulls<>(new IntObjectHashMap<>());
+                return () -> new PrimitiveMapWithNulls(new Int2ObjectOpenCustomHashMap(IntHashStrategy.INSTANCE));
 
             case LongType.ID:
             case TimestampType.ID_WITH_TZ:
             case TimestampType.ID_WITHOUT_TZ:
-                return () -> (Map) new PrimitiveMapWithNulls<>(new LongObjectHashMap<>());
+
+                return () -> new PrimitiveMapWithNulls(new Long2ObjectOpenCustomHashMap<V>(LongHashStrategy.INSTANCE));
 
             default:
-                return HashMap::new;
+                return Object2ObjectOpenHashMap::new;
         }
     }
 }
